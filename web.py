650993c987ddb8d88403b5e9760651da32676c9a
lordgabu <vinodsharma98176@gmail.com>
2025-07-07 22:30:34 +0530

    Update web.py

--- web.py
+++ web.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Bot is running by SPIDY UNIVERSE"
+    return "Bot is running by GABU BHAIYA"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
